b399c30c525b1b9b480ee89edae4a6582b7d40f7
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2022-06-13 17:29:19 -0700

    Add data.js file for slide data used in app.

---
+++ src/data.js
@@ -0,0 +1,92 @@
+export const slides = [
+  {
+    id: "iST4DK",
+    name: "Opening lines",
+    imageUrl:
+      "https://live.staticflickr.com/65535/52115785409_cb3e231f9c_o.png",
+    book: "Inferno",
+    canto: "1",
+    line: "1",
+  },
+  {
+    id: "0ycGBN",
+    name: "Lion",
+    imageUrl:
+      "https://live.staticflickr.com/65535/52114512102_927f1c4959_o.png",
+    book: "Inferno",
+    canto: "1",
+    line: "43",
+  },
+  {
+    id: "pzhqxU",
+    name: "Shewolf",
+    imageUrl:
+      "https://live.staticflickr.com/65535/52115542796_8c0323677b_o.png",
+    book: "Inferno",
+    canto: "1",
+    line: "88",
+  },
+  {
+    id: "fXsOvJ",
+    name: "Heavens",
+    imageUrl:
+      "https://live.staticflickr.com/65535/52115782084_8a317f3ffe_o.png",
+    book: "Purgatorio",
+    canto: "1",
+    line: "22",
+  },
+  {
+    id: "7Royz9",
+    name: "Cato",
+    imageUrl:
+      "https://live.staticflickr.com/65535/52115784899_c349a8a6bd_o.png",
+    book: "Purgatorio",
+    canto: "1",
+    line: "49",
+  },
+  {
+    id: "FPgI5D",
+    name: "Arrival of souls",
+    imageUrl:
+      "https://live.staticflickr.com/65535/52115581628_246c2350b3_o.png",
+    book: "Purgatorio",
+    canto: "2",
+    line: "28",
+  },
+  {
+    id: "gkP0fw",
+    name: "Piccardia",
+    imageUrl:
+      "https://live.staticflickr.com/65535/52115783994_15b2e46506_o.png",
+    book: "Paradiso",
+    canto: "3",
+    line: "49",
+  },
+  {
+    id: "zd8o0u",
+    name: "Glowing souls",
+    imageUrl:
+      "https://live.staticflickr.com/65535/52115540436_c82124bf0c_o.png",
+    book: "Paradiso",
+    canto: "5",
+    line: "100",
+  },
+  {
+    id: "uDnV4U",
+    name: "Charles Martel",
+    imageUrl:
+      "https://live.staticflickr.com/65535/52116047610_9ee2eb4d26_o.png",
+    book: "Paradiso",
+    canto: "8",
+    line: "49",
+  },
+  {
+    id: "Eklw6j",
+    name: "Glowing souls",
+    imageUrl:
+      "https://live.staticflickr.com/65535/52114513277_cf194c5fdc_o.png",
+    book: "Paradiso",
+    canto: "12",
+    line: "19",
+  },
+];
